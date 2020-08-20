2799f3fce9f9ef04dcb953c1f68766c2fb0aec8b
DEV-PROG-specs <devendra.srivastava_cs19@gla.ac.in>
2020-08-20 11:51:50 -0700

    create toString() Method in Video.java

--- src/definations/Video.java
+++ src/definations/Video.java
@@ -1,9 +1,30 @@
 package definations;
 
+import java.util.Objects;
+
 public class Video {
     private String videoName;
     private boolean checkOut;
     private int rating;
+
+    //Constructor:Used to initialize the values of fields
+    public Video() {
+        videoName = "War";
+        rating = 5;
+        checkOut = true;
+    }
+
+    public Video(String videoName, int rating, boolean checkOut) {
+        this.videoName = videoName;
+        this.rating = rating;
+        this.checkOut = checkOut;
+    }
+
+    public Video(String videoName) {
+        this.videoName = videoName;
+        rating = 3;
+        checkOut = false;
+    }
     // Some specials types of methods in java
 
     //1.The getter method:Used to read the value of private fields.
@@ -33,5 +54,24 @@ public class Video {
 
     public void setCheckOut(boolean checkOut) {
         this.checkOut = checkOut;
+    }
+
+    public String toString() {
+        return String.format("Video Name=%s,Video Rating=%d,Is video checkout=%b", getVideoName(), getRating(), isCheckOut());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Video video = (Video) o;
+        return isCheckOut() == video.isCheckOut() &&
+                getRating() == video.getRating() &&
+                Objects.equals(getVideoName(), video.getVideoName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getVideoName(), isCheckOut(), getRating());
     }
 }
--- src/execution/Main.java
+++ src/execution/Main.java
@@ -5,13 +5,11 @@ import definations.Video;
 
 public class Main {
     public static void main(String[] args) {
-        Video video = new Video();
-        video.setVideoName("Baahubali");
-        video.setRating(5);
-        video.setCheckOut(true);
-        System.out.println(video.getVideoName());
-        System.out.println(video.getRating());
-        System.out.println(video.isCheckOut());
+        Video video = new Video("Robot");
+        Video video2 = new Video("Dum");
+        System.out.println(video);
+        System.out.println(video.equals(video2));
+
 
     }
 }

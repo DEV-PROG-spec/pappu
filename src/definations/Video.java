8fbca88683457d04fd4f26a79ce11ee0e1243d19
DEV-PROG-specs <devendra.srivastava_cs19@gla.ac.in>
2020-08-20 10:39:28 -0700

    create getter and setter for Video.java

--- src/definations/Video.java
+++ src/definations/Video.java
@@ -4,6 +4,34 @@ public class Video {
     private String videoName;
     private boolean checkOut;
     private int rating;
+    // Some specials types of methods in java
 
+    //1.The getter method:Used to read the value of private fields.
+    //Observer method / Accessor method
+    public String getVideoName() {
+        return videoName;
+    }
 
+    //2.The setter method:Used to write the value of private fields.
+    //Transformer method / Mutator method.
+    public void setVideoName(String videoName) {
+        this.videoName = videoName;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+    //exception for booleans: name of getter method start with is .
+
+    public boolean isCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(boolean checkOut) {
+        this.checkOut = checkOut;
+    }
 }
--- src/execution/Main.java
+++ src/execution/Main.java
@@ -6,7 +6,12 @@ import definations.Video;
 public class Main {
     public static void main(String[] args) {
         Video video = new Video();
-
+        video.setVideoName("Baahubali");
+        video.setRating(5);
+        video.setCheckOut(true);
+        System.out.println(video.getVideoName());
+        System.out.println(video.getRating());
+        System.out.println(video.isCheckOut());
 
     }
 }

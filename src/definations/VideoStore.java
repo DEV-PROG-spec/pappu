e68479fb553582e369708841fff108cf45878c23
DEV-PROG-specs <devendra.srivastava_cs19@gla.ac.in>
2020-08-21 10:10:47 -0700

    add constructors in VideoStore.java

--- src/definations/VideoStore.java
+++ src/definations/VideoStore.java
@@ -3,6 +3,17 @@ package definations;
 public class VideoStore {
     private Video[] store;
 
+    public VideoStore() {
+        this.store = new Video[10];
+        for (int i = 0; i < store.length; i++) {
+            store[i] = new Video();
+        }
+    }
+
+    public VideoStore(Video[] store) {
+        this.store = store;
+    }
+
     public Video[] getStore() {
         return store;
     }

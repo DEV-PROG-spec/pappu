a5ae3e171879a5d55cd2c83ec7df9e15b18f4142
DEV-PROG-specs <devendra.srivastava_cs19@gla.ac.in>
2020-08-21 09:57:30 -0700

    add getters and setters for fields of VideoStore.java

--- src/definations/VideoStore.java
+++ src/definations/VideoStore.java
@@ -3,4 +3,11 @@ package definations;
 public class VideoStore {
     private Video[] store;
 
+    public Video[] getStore() {
+        return store;
+    }
+
+    public void setStore(Video[] store) {
+        this.store = store;
+    }
 }
